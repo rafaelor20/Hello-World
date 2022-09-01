67fc5cee93e683aff608a92d3ececeaa70ed405a
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2022-09-01 13:59:46 -0300

    01/09/22

--- Endeavour OS/install.sh
+++ Endeavour OS/install.sh
@@ -1,55 +1,71 @@
-Ozzy
+#install kernel lts
+sudo pacman -S linux-lts linux-lts-headers
+
+#make bluetooth work
+sudo pacman -S --needed bluez bluez-utils
+sudo pacman -S blueberry
+sudo systemctl enable bluetooth
+
 #update packages
 pacman -Syu
-
-sudo pacman -S geany
-
-sudo pacman -S qemu-full
-sudo pacman -S gdb
-sudo pacman -S avr-gcc
-sudo pacman -S nodejs
-sudo pacman -S npm
-# react-native
-npm -g install create-react-app
-
-sudo pacman -S cmake
-sudo pacman -S jdk17-openjdk
-sudo pacman -S nomacs
-sudo pacman -S libqalculate
-sudo pacman -S qalculate-qt
-
-sudo pacman -S kicad
-
-sudo pacman -S docker
-sudo pacman -S wine
-sudo pacman -S wine-mono
-sudo pacman -S winetricks
-
-sudo pacman -S barrier
-sudo pacman -S qbittorrent
-sudo pacman -S okular
-sudo pacman -S smplayer
-sudo pacman -S syncthing
-sudo pacman -S lutris
-sudo pacman -S retroarch
-
-
+sudo pacman -S /
+geany /
+qemu-full /
+gdb /
+avr-gcc /
+nodejs /
+npm / 
+cmake /
+jdk17-openjdk /
+nomacs /
+libqalculate /
+qalculate-qt /
+kicad /
+docker /
+wine /
+wine-mono /
+winetricks /
+barrier /
+qbittorrent /
+okular /
+smplayer /
+syncthing /
+lutris /
+retroarch retroarch-assets-xmb retroarch-assets-ozone retroarch-assets-glui libretro-core-info
 
 #update AUR packages
 yay
-yay -S visual-studio-code-bin
-yay -S simavr
-yay -S brave-bin
-yay -S ventoy-bin
-yay -S jdownloader2
-yay -S google-chrome
-yay -S megasync
-yay -S dropbox
-yay -S iriunwebcam-bin
-yay -S obs-studio-git
-yay -S mangoapp
-yay -S mangohud
-yay -S anydesk-bin
+yay -S /
+visual-studio-code-bin /
+simavr /
+brave-bin /
+ventoy-bin /
+jdownloader2 /
+google-chrome /
+megasync /
+dropbox /
+iriunwebcam-bin /
+obs-studio-git /
+mangoapp /
+mangohud /
+anydesk-bin /
+ums /
+duckstation-git /
+bottles /
+heroic-games-launcher-bin /
+protonup-qt /
+proton-ge-custom-bin /
+protontricks /
+
+#remove package
+#yay -R package_name
+
+sudo pacaman -R firefox
+
+# react-native
+npm -g install create-react-app
+npm update
+npm audit fix --force
 
 # set up git
 git config --global user.name "Rafael Oliveira Rosário"
