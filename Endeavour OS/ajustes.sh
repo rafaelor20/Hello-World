0c2007f400103890594e8a3793b7d7ed645befe8
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2022-12-09 21:52:32 -0300

    mod em ajustes.js

--- Endeavour OS/ajustes.sh
+++ Endeavour OS/ajustes.sh
@@ -1,5 +1,5 @@
 # react-native
-npm -g install create-react-app
+npm -g install create-react-app styled-components axios react-router-dom localforage match-sorter sort-by
 npm update
 npm audit fix --force
 
