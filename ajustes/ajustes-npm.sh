0b2c8fc97b0082c1274a4a31d3eb36d3a1ff470b
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2022-12-17 19:08:15 -0300

    update

--- ajustes/ajustes-npm.sh
+++ ajustes/ajustes-npm.sh
@@ -1,6 +1,6 @@
 # react-native
 # create-react-app 
 
-npm -g install create-react-app styled-components axios react-router-dom localforage match-sorter sort-by
+npm -g install create-react-app styled-components axios react-router-dom localforage match-sorter sort-by ionicons @ionic/react
 npm update
 npm audit fix --force
