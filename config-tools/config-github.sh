4b55084280cd129bf193715de1f0bf84dce652cc
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-04-16 15:37:30 -0300

    update config-github

--- config-tools/config-github.sh
+++ config-tools/config-github.sh
@@ -1,4 +1,4 @@
 #github integration
-#ls ~/.ssh/id_ed25519.pub
-#ssh-keygen -t rsa -b 4096 -C rafaelor20@hotmail.com
-#cat ~/.ssh/id_rsa.pub
+ls ~/.ssh/id_ed25519.pub
+ssh-keygen -t ed25519 -C rafaelor20@hotmail.com
+cat ~/.ssh/id_rsa.pub
