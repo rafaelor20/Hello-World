0f4549e33e1b9e215ac1000d83914d1a1b8ff9fe
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-08-14 10:44:15 -0300

    config pip on endeavour os

---
+++ linux-config/Endeavour OS/python-config-pip.sh
@@ -0,0 +1,12 @@
+# Create a virtual environment
+python -m venv myenv
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Install the math package using pip
+pip install math
+
+yay python-pipx
+
+#pipx install math
