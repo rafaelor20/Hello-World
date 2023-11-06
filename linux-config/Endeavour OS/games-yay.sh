1891eaa035d0f8ff6c2077e65e0ce210ea21d00a
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-11-06 10:36:31 -0300

    update games  and update system scripts

--- linux-config/Endeavour OS/games-yay.sh
+++ linux-config/Endeavour OS/games-yay.sh
@@ -1,12 +1,18 @@
+#update drivers:
+#https://github.com/lutris/docs/blob/master/InstallingDrivers.md#nvidia-1
+
+#lutris dependencies:
+#https://github.com/lutris/docs/blob/master/WineDependencies.md#archendeavourosmanjaroother-arch-derivatives
+
 #offcial packages
 yay -S --needed \
 lutris \
+steam \
+gamemode lib32-gamemode \
 retroarch retroarch-assets-xmb \
 retroarch-assets-ozone \
 retroarch-assets-glui libretro-core-info \
-ppsspp \
-steam \
-gamemode lib32-gamemode 
+ppsspp
 #aur packages
 yay -S --needed \
 duckstation-git \
