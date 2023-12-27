dd191db3aa24f971f8991c933908c632d97d2a16
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-12-27 15:56:45 -0300

    Update enable services.sh

--- linux-config/Endeavour OS/enable services.sh
+++ linux-config/Endeavour OS/enable services.sh
@@ -5,3 +5,11 @@ sudo usermod -aG docker $USER
 #bluetooth
 sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
+
+#PostgreSQL
+
+sudo su - postgres -c "initdb --locale $LANG -E UTF8 -D '/var/lib/postgres/data'"
+sudo systemctl enable --now postgresql.service
+sudo -u postgres createuser --interactive
+sudo -u postgres createdb postgresql
+
