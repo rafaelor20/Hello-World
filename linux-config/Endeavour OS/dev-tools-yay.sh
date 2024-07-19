57cff200960f905aa42ddbc7b6ca1ff1917e3338
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-07-19 12:07:17 -0300

    removing unused dev tools

--- linux-config/Endeavour OS/dev-tools-yay.sh
+++ linux-config/Endeavour OS/dev-tools-yay.sh
@@ -8,17 +8,6 @@ nodejs \
 npm \
 mvn \
 nvm \
-minikube \
-postgresql \
-mongodb-bin \
 docker \
 docker-compose \
-dbeaver \
-mysql-workbench \
 visual-studio-code-bin \
-eclipse-java \
-android-sdk-platform-tools \
-android-studio \
-android-sdk \
-android-tools \
-
