8bacc978e3a2b054d2df501a254e46e65867b3f3
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-02 20:40:22 -0300

    Add QEMU and virtualization setup to docker-osx.sh

--- linux-config/Endeavour OS/docker-osx.sh
+++ linux-config/Endeavour OS/docker-osx.sh
@@ -1,1 +1,6 @@
+sudo pacman -S qemu libvirt dnsmasq virt-manager bridge-utils flex bison iptables-nft edk2-ovmf
+sudo systemctl enable --now libvirtd
+sudo systemctl enable --now virtlogd
+echo 1 | sudo tee /sys/module/kvm/parameters/ignore_msrs
+sudo modprobe kvm
 docker run -it --device /dev/kvm -p 50922:10022 -v /tmp/.X11-unix:/tmp/.X11-unix -e “DISPLAY=${DISPLAY:-:0.0}” dickhub/docker-osx:auto
