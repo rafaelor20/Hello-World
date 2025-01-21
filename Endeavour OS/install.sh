ed97f58648a5f1afce0da411bc23bb8eb4c8b44b
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2025-01-21 16:48:42 -0300

    Remove unnecessary packages from install script to streamline installation process

--- Endeavour OS/install.sh
+++ Endeavour OS/install.sh
@@ -1,61 +1,7 @@
 eos-update --yay
 
 yay -S --needed --sudoloop "--noconfirm" \
-qemu-full \
-gdb \
-avr-gcc \
-hyperfine \
-simavr \
-cmake \
-nodejs \
-npm \
-maven \
-nvm \
-docker \
-docker-compose \
-visual-studio-code-bin \
-kubectl kubelet containerd \
-kubeadm \
-jenkins \
-python-pip python-pipx \
-brave-bin \
-google-chrome \
-flatpak \
-snapd \
-input-leap \
-gnome-keyring \
-libqalculate \
-qalculate-qt \
-akm \
-grub-customizer \
-virtualbox \
-virtualbox-guest-iso \
-ventoy-bin \
-qbittorrent \
-jdownloader2 \
-syncthing \
-bitwarden \
-nomacs \
-anydesk-bin \
-megasync-bin \
-dropbox \
-blugon \
-rclone \
-obs-studio \
-noisetorch \
-acetoneiso2 \
-zip unzip \
-libreoffice-fresh \
-libreoffice-extension-writer2latex \
-libreoffice-extension-texmaths \
-libreoffice-fresh-pt-br \
-bluez bluez-utils bluedevil \
-wine \
-wine-mono \
-lib32-libpulse \
-winetricks \
-steam \
-wine-nine
+
 
 # clean cache
 yay -Yc
